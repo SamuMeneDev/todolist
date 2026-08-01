e908ffd432b1a3fdb5196791fb6a2833852dcccc
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-08-01 00:09:39 -0300

    feat: Angular Material escolhido

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,13 +1,18 @@
-import { TuiRoot } from '@taiga-ui/core';
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('todolist-web');
+
+  olaFn() {
+    console.log("Olá mundo!");
+  }
+
 }
