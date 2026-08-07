2cf6f19c1272ec234628292d1bfbb1082fabe16c
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-08-07 13:49:15 -0300

    feat: Estilzação da navbar e começo do formulario

--- apps/web/src/app/_pages/login/login.ts
+++ apps/web/src/app/_pages/login/login.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "@/app/_components";
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, MatFormField, MatInputModule, Navbar],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
