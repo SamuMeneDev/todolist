9acbed18a1b49729f95800235434ffc7560a159b
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-08-07 22:25:16 -0300

    feat: Layout de login finalizado

--- apps/web/src/app/_pages/login/login.ts
+++ apps/web/src/app/_pages/login/login.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from "@/app/_components";
 import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterOutlet, MatFormField, MatInputModule, Navbar],
+  imports: [RouterOutlet, MatFormField, MatInputModule, MatButtonModule, Navbar],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
